a29094e76dfd4966bacba40fb3b56b0178462e83
iTAP <iTAP@iTAP-PC18>
2015-05-07 17:19:42 +0900

    マイナスクラス完成(仮)後々編集予定

--- src/try_github/minus.java
+++ src/try_github/minus.java
@@ -1,7 +1,19 @@
 package try_github;
 
+
+
 public class minus {
+
+	String c;
+	int d=0;
 	public void transition(int a, int b){
+		if("-".equals(c)){
+	d=	a-b;
+		}
+	}
+
+	public void say(){
+		System.out.println(d);
 	}
 
 }
